41f95a82dac5b100f6dc6955bea1afb25d2bd456
Edwin Oliveira <39379560+EdwinOliveira@users.noreply.github.com>
2020-05-09 11:58:42 +0100

    Test Example

--- tests/EndPointsStubTests.java
+++ tests/EndPointsStubTests.java
@@ -5,9 +5,11 @@ import org.junit.Test;
 import stubs.EndPointsStub;
 import types.User;
 
+import java.util.HashMap;
+
 public class EndPointsStubTests {
     @Test
-   public void addUser_UserContainsAllFields_Status201AndNewUser() {
+   public void addUser_Status201AndNewUser() {
         final EndPointsStub endPointsStub = new EndPointsStub();
 
         final User user = new User(1, "xX69FalcaoZinhoGamer69Xx@outlook.pt", "Diogo", "Falcao", "TiagovskiCoolXxX6969");
@@ -15,5 +17,7 @@ public class EndPointsStubTests {
         final String stubAnswer = endPointsStub.addUser(user);
 
         System.out.println(stubAnswer);
+
+        assertEquals("{\"body\":{\"First_Name\":\"Diogo\",\"Email\":\"xX69FalcaoZinhoGamer69Xx@outlook.pt\",\"Last_Name\":\"Falcao\",\"Id\":1,\"Avatar\":\"TiagovskiCoolXxX6969\"},\"status\":201}", stubAnswer);
     }
 }
