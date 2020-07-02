bda0822f8f9943378c49b7acf154026190b03b3c
Diogo Oliveira <diogoxd97@hotmail.com>
2020-07-02 16:24:01 +0100

    cmmit

--- src/main/java/cache/Resources.java
+++ src/main/java/cache/Resources.java
@@ -6,18 +6,25 @@ import types.Resource;
 import java.util.ArrayList;
 
 public class Resources {
-    private ArrayList<Resource> resources = new ArrayList<Resource>();
     private final ResourcesInterface resourcesInterface;
 
     public Resources(ResourcesInterface resourcesInterface) {
         this.resourcesInterface = resourcesInterface;
     }
 
-    public Resource listResource(Integer id) {
-        return null;
+    public Resource listResource(Integer id) throws Exception {
+        if((id instanceof Integer) == false) {
+            throw new Exception("Id must be integer!");
+        }
+
+        if(id == null) {
+            throw new Exception("Id can't be null!");
+        }
+
+        return this.resourcesInterface.getResource(id);
     }
 
     public ArrayList<Resource> listResources() {
-        return null;
+        return this.resourcesInterface.getResources();
     }
 }
--- tests/application/ResourcesTests.java
+++ tests/application/ResourcesTests.java
@@ -1,4 +1,49 @@
 package application;
 
+import cache.Resources;
+import cache.Users;
+import org.junit.Test;
+import stubs.ResourcesStub;
+import stubs.UsersStub;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ResourcesTests {
+    // List Resources
+
+    @Test
+    public void listResources_ResourcesFound_MustNotThrowException() {
+        assertDoesNotThrow(() -> {
+            new Resources(new ResourcesStub()).listResources();
+        });
+    }
+
+    @Test
+    public void listResources_ResourcesNotFound_MustReturnNull() {
+        //assertNull(new Resources(new ResourcesStub()).listResources());
+    }
+
+    // List Resource
+
+    @Test
+    public void listResource_IdTypeIsInteger_MustNotThrowException() {
+        assertDoesNotThrow(() -> {
+            new Resources(new ResourcesStub()).listResource(1);
+        });
+    }
+
+    @Test
+    public void listResource_IdIsNotNull_MustNotThrowException() {
+        assertDoesNotThrow(() -> {
+            new Users(new UsersStub()).listUser(1);
+        });
+    }
+
+    @Test
+    public void listResource_IdIsNull_MustThrowException() {
+        assertThrows(Exception.class, () -> {
+            new Users(new UsersStub()).listUser(null);
+        });
+    }
 }
